3c6cf6a37c1f2d18d6ca813d03d5b3ea11149943
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 12:17:46 +0530

    Update schemas.js

--- timeline/schemas.js
+++ timeline/schemas.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const timeline = {
-  timeline: {
-    body: {
-      type: 'object',
-      required: [ ],
-      properties: { },
-      additionalProperties: false
+const timeline = {              // timeline object is created
+  timeline: {                   // timeline onject inside timeline object
+    body: {                    // body object inside timeline
+      type: 'object',           // type parameter set to object
+      required: [ ],        // requird is another parameter , empty array
+      properties: { },        // properties is empty object 
+      additionalProperties: false   // additional properties are set to false
     }
   }
 }
 
-module.exports = {
+module.exports = {       // timeline object is exported
   timeline
 }
