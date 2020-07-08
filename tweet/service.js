ed298e78337613331d24db8c3755a3426e71dcca
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 13:21:50 +0530

    Update service.js

--- tweet/service.js
+++ tweet/service.js
@@ -1,42 +1,42 @@
 'use strict'
 
-function convertUserIdToStringInTweet (t) {
+function convertUserIdToStringInTweet (t) {               //function to convert no to string
   t.user._id = t.user._id.toString('hex')
   return t
 }
 
-class TweetService {
-  constructor (tweetCollection) {
+class TweetService {                                    //TweetService class is defined
+  constructor (tweetCollection) {                       //constructor takes tweetCollection and set it as a parameter
     this.tweetCollection = tweetCollection
   }
 
-  async fetchTweets (userIds) {
-    const tweets = await this.tweetCollection.find({
+  async fetchTweets (userIds) {                            //fetchTweets function is defined
+    const tweets = await this.tweetCollection.find({        //tweets with a passed userIds are found and sorted in an array
       'user._id': { $in: userIds }
     }).sort({createdAt: -1}).toArray()
-    return tweets.map(convertUserIdToStringInTweet)
+    return tweets.map(convertUserIdToStringInTweet)         //tweets array is mapped with function and new array is returned
   }
 
-  async addTweet (user, text) {
-    await this.tweetCollection.insertOne({
+  async addTweet (user, text) {                           //addTweet function is defined
+    await this.tweetCollection.insertOne({                //new tweet with user, text and date is added
       user,
       text,
       createdAt: new Date()
     })
   }
 
-  async ensureIndexes (db) {
-    await db.command({
+  async ensureIndexes (db) {                              //function is defined
+    await db.command({                                      //db command
       'collMod': this.tweetCollection.collectionName,
-      validator: {
+      validator: {                                        //valid object to validate type 
         user: { $type: 'object' },
         'user._id': { $type: 'string' },
         'user.username': { $type: 'string' },
         text: { $type: 'string' }
       }
     })
-    await this.tweetCollection.createIndex({ 'user._id': 1 })
+    await this.tweetCollection.createIndex({ 'user._id': 1 })   
   }
 }
 
-module.exports = TweetService
+module.exports = TweetService             //TweetService is exported
