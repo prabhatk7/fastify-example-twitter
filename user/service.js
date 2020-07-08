1b78a6bcdee0c60b22c4f1f61da62e8072852ebb
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 14:15:28 +0530

    Update service.js

--- user/service.js
+++ user/service.js
@@ -1,42 +1,42 @@
 'use strict'
 
-const DUPLICATE_KEY_ERROR_CODE = 11000
+const DUPLICATE_KEY_ERROR_CODE = 11000        
 
-const errors = require('../errors')
+const errors = require('../errors')                 //errors is imported from '../errors'   
 
-class UserService {
+class UserService {                                //UserService object is created
   constructor (userCollection) {
     this.userCollection = userCollection
   }
 
-  async register (username, password) {
+  async register (username, password) {           //register method is written
     let writeResult
-    try {
+    try {                                         //try to execute insert user in userCollection
       writeResult = await this.userCollection.insertOne({ username, password })
-    } catch (e) {
+    } catch (e) {                                     //catch the error
       if (e.code === DUPLICATE_KEY_ERROR_CODE) {
-        throw new Error(errors.USERNAME_IS_NOT_AVAILABLE)
+        throw new Error(errors.USERNAME_IS_NOT_AVAILABLE)        //throw new error if e.code is 11000
       }
-      throw e
+      throw e                                               //else throw the same error
     }
 
-    return writeResult.insertedId
+    return writeResult.insertedId                 //if no error, return insertedId
   }
 
-  async login (username, password) {
+  async login (username, password) {                     //login method is written
     const users = await this.userCollection.find({ username, password }, { projection: {password: 0} }).toArray()
     const user = users[0]
 
     if (!user) throw new Error(errors.WRONG_CREDENTIAL)
 
-    return user
+    return user          //return user
   }
 
-  getProfile (_id) {
+  getProfile (_id) {                              //getProfile method
     return this.userCollection.findOne({ _id }, { projection: {password: 0} })
   }
 
-  async search (searchString) {
+  async search (searchString) {                //search method
     const query = {
       username: { $regex: searchString }
     }
@@ -44,7 +44,7 @@ class UserService {
     return users
   }
 
-  async ensureIndexes (db) {
+  async ensureIndexes (db) {                          //ensureIndexes method 
     await db.command({
       'collMod': this.userCollection.collectionName,
       validator: {
@@ -56,4 +56,4 @@ class UserService {
   }
 }
 
-module.exports = UserService
+module.exports = UserService              //userService exported
