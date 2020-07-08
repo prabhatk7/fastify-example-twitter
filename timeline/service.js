6b58281233a5fd3129c5c9935cd78e3c6c1cdd33
Prabhat Kumar <60351919+prabhatk7@users.noreply.github.com>
2020-07-08 12:39:16 +0530

    Update service.js

--- timeline/service.js
+++ timeline/service.js
@@ -1,16 +1,16 @@
 'use strict'
 
-class TimelineService {
-  constructor (followClient, tweetClient) {
+class TimelineService {                        //TimelineService class is created
+  constructor (followClient, tweetClient) {   // constructor takes two arguments and set two parameters for object
     this.followClient = followClient
     this.tweetClient = tweetClient
   }
 
-  async getTimeline (userId) {
-    const followerIds = await this.followClient.getFollowing(userId)
-    followerIds.push(userId)
-    return this.tweetClient.fetchTweets(followerIds)
+  async getTimeline (userId) {                                           //getTimeline function takes userId as arg
+    const followerIds = await this.followClient.getFollowing(userId)     //followerIds variable is initialized 
+    followerIds.push(userId)                                             //followeIds pudh method is executed
+    return this.tweetClient.fetchTweets(followerIds)                    //followeIds is passed to fetchTweets
   }
 }
 
-module.exports = TimelineService
+module.exports = TimelineService              //TimelineService class is exported
